72d4457cd29952902ef84e60829e5c8c07636112
Jay Zeng <jayzeng@jay-zeng.com>
2022-12-11 15:26:46 -0800

    * debug

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from src.api import Rpilocator, RpilocatorMock
 def main():
     # rpilocator = Rpilocator('us')
     # print(rpilocator.send())
+    pass
 
-    rpilocator = RpilocatorMock('us')
-    data = rpilocator.send()
-    print(data)
+    # rpilocator = RpilocatorMock('us')
+    # data = rpilocator.send()
+    # print(data)
 
 if __name__ == "__main__":
     main()
